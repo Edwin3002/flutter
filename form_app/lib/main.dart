51a172c43b2903af0439e405042bf2a6301abcb9
Edwin Vargas <vargas11742@gmail.com>
2023-11-14 23:48:12 -0500

    formsApp

---
+++ form_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: Row(children: [
+          Text('Form app'),
+          Text(Platform.isAndroid ? "android" : "ios"),
+        ])),
+      ),
+    );
+  }
+}
